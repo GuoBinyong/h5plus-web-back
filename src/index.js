5b4abacc8fe98a992c92419c564736267bf8f130
郭斌勇 <guobinyong@qq.com>
2020-05-18 15:37:09 +0800

    移除非html5+的东西

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-export * from './ConditionBack.js' ;
-export * from './WebviewBack.js';
-export * from './BackScheme.js'
+export * from './WebviewBack';
 export * as Actions from './Action.js';
-export * as Conditions from './Condition.js';
-
-import {BackScheme} from "./BackScheme";
-export default BackScheme;
